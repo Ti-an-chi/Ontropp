4da46865bdd96b51efea4fe5975b6d7dc7ffc181
Fredericktokede <fredericktokede@gmail.com>
2026-07-04 19:36:58 +0100

    update

--- api.js
+++ api.js
@@ -5,9 +5,10 @@ const API = {
   requestCount: 0,
   
   basURL: 'http://localhost:8787',
+
   basedURL: 'https://ontropp-backend.onrender.com/api',
-  baseURL: 'https://ontrop-api.dsub.workers.dev',
-  
+
+  baseURL: 'https://ontrop-api.dsub.workers.dev',   
   // Store tokens & userId after login
   setTokens({ accessToken, refreshToken, userId }) {
     localStorage.setItem('ontrop_token', accessToken);
